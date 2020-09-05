7d75c93a815ffa8f861800aaed1eae11feb58047
toannn-2309 <nguyen.ngoc.toan@sun-asterisk.com>
2020-09-05 10:24:01 +0700

    Chapter_9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      check_remember params[:session][:remember_me], user
       redirect_to user
     else
       flash[:danger] = t "session.noti.danger"
@@ -13,7 +14,13 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def check_remember remember_me, user
+    remember_me == Settings.session.remember ? remember(user) : forget(user)
   end
 end
